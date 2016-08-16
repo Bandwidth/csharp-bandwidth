e3ed5e574e3a052c17216fbd3d679d76c13a1998
Unknown <belchikovs@gmail.com>
2016-08-16 11:17:14 +0300

    Added missing test

--- test/Bandwidth.Net.Test/Api/ConferenceTest.cs
+++ test/Bandwidth.Net.Test/Api/ConferenceTest.cs
@@ -163,6 +163,18 @@ namespace Bandwidth.Net.Test.Api
       await api.UpdateMemberAsync("id", "memberId", new UpdateConferenceMemberData{State = ConferenceMemberState.Completed});
     }
 
+    [Fact]
+    public async void TestPlayToMemberAudio()
+    {
+      var context = new MockContext<IHttp>();
+      context.Arrange(
+        m =>
+          m.SendAsync(The<HttpRequestMessage>.Is(r => IsValidPlayAudioToMemberRequest(r)), HttpCompletionOption.ResponseContentRead,
+            null)).Returns(Task.FromResult(new HttpResponseMessage()));
+      var api = Helpers.GetClient(context).Conference;
+      await api.PlayAudioToMemberAsync("id", "memberId", new PlayAudioData { FileUrl = "url" });
+    }
+
     public static bool IsValidCreateRequest(HttpRequestMessage request)
     {
       return request.Method == HttpMethod.Post && request.RequestUri.PathAndQuery == "/v1/users/userId/conferences" &&
@@ -185,6 +197,13 @@ namespace Bandwidth.Net.Test.Api
     public static bool IsValidPlayAudioRequest(HttpRequestMessage request)
     {
       return request.Method == HttpMethod.Post && request.RequestUri.PathAndQuery == "/v1/users/userId/conferences/id/audio" &&
+             request.Content.Headers.ContentType.MediaType == "application/json" &&
+             request.Content.ReadAsStringAsync().Result == "{\"fileUrl\":\"url\"}";
+    }
+
+    public static bool IsValidPlayAudioToMemberRequest(HttpRequestMessage request)
+    {
+      return request.Method == HttpMethod.Post && request.RequestUri.PathAndQuery == "/v1/users/userId/conferences/id/members/memberId/audio" &&
              request.Content.Headers.ContentType.MediaType == "application/json" &&
              request.Content.ReadAsStringAsync().Result == "{\"fileUrl\":\"url\"}";
     }
